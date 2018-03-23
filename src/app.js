4ee42c079e7466269c68e3b1c058e2c87004d5fc
Carlos García <deivicarlos@gmail.com>
2018-03-22 20:22:25 -0400

    Updates

--- src/app.js
+++ src/app.js
@@ -1,1 +1,12 @@
-console.log('app.js is running!!!');
+
+//import './utils.js';
+
+import validator from 'validator';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+//console.log(isAdult(17));
+//console.log(canDrink(22));
+
+const template = React.createElement('p', {}, 'testing!!!!');
+ReactDOM.render(template, document.getElementById('App'));
