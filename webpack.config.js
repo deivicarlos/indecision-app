9277613281906c9a83b69b8155cfc4b9bb9ac17a
Carlos García <deivicarlos@gmail.com>
2018-03-27 17:12:46 -0400

    Updates

--- webpack.config.js
+++ webpack.config.js
@@ -5,5 +5,15 @@ module.exports = {
     output: {
         path: path.join(__dirname, 'public'),   //provide absolute path
         filename: 'bundle.js'
+    },
+    module: {
+        rules: [{
+            loader: 'babel-loader',
+            test: /\.js$/,
+            exclude: /node_modules/
+        }]
     }
 };
+
+
+//loader 
